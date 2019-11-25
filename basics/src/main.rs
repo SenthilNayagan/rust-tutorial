c7703fec5af37310ee151838eee4bc5c8596a67b
senthilnayagan <senthil.nayagan@gmail.com>
2019-11-25 13:58:51 +0530

    Updated

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Rust!");
+
+    // Compound Types - Arrays
+    // Unlike a tuple, every element of an array must have the same type.
+    let a_val = [1, 2, 3, 4, 5];
+    println!("{}", a_val[2]);
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    println!("{}", months[3]);
+
+    // Compound Types - Tuples
+    // Tuples can have variety of types as shown below
+    let tup_val: (i32, f64, u8) = (10, 6.5, 1);
+    println!("{}", tup_val.1);
+
+
+
 }
